ba3a825b3cc1f5d61abaccbb3429fd1ec661b4b9
Nicolas Heredia <nicoherediasp@gmail.com>
2023-03-13 18:17:06 -0300

    Eliminado ahora 18 y cambio en montos de Naranja en 3 y 6 cuotas

--- js/app.js
+++ js/app.js
@@ -6,16 +6,14 @@ const form = document.querySelector("#form")
 
 //objeto que contiene los tasas que se cobran dependiendo cada tarjeta y cuota deseada    
 const montoFinal_ahora = {
-    naranja : 1.115448,
+    debito : 1.07009,
     cuotas3 : 1.242546,
     cuotas6 : 1.380454,
-    cuotas12 : 1.706776,
-    cuotas18 : 2.119092
+    cuotas12 : 1.706776
 }
 const montoFinal_naranja = {
-    debito : 1.07009,
-    naranja3 :montoFinal_ahora.naranja * 1.2614,
-    naranja6 :montoFinal_ahora.naranja * 1.4834
+    naranja3 : 1.398602,
+    naranja6 : 1.683502
 }
 
 //Funciones que se encargan de crear las tarjetas y los titulos
@@ -29,7 +27,7 @@ const tarjetaCuotas = (montoFinal,cuota) => {
     return plantilla 
 }
 
-const tarjetaDebitoYPlanZ = (montoFinal, cuota, nombre) => {
+const tarjetaDebito = (montoFinal, cuota, nombre) => {
     let plantilla = `<div class="cardCuota fade-in">
     <div class="cardCuota__numero">${cuota}</div>
     <div class="cardCuota__contenedorMontos">
@@ -57,10 +55,10 @@ form.addEventListener("click", (e) =>{
     } else if (e.target.id === "btn-ahora"){
     resultadoCuotas.innerHTML = "";
     resultadoCuotas.innerHTML += crearTitulo("Plan Ahora")
+    resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.debito, "D", "Precio final Debito")
     resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas3, 3);
     resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas6, 6);
     resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas12, 12);
-    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas18, 18);
     
     } else if (e.target.id === "btn-borrar"){
         let borrarDelay
@@ -77,9 +75,7 @@ form.addEventListener("click", (e) =>{
         }
     } else if (e.target.id === "btn-naranja") {
         resultadoCuotas.innerHTML = "";
-        resultadoCuotas.innerHTML += crearTitulo("Naranja Z")
-        resultadoCuotas.innerHTML += tarjetaDebitoYPlanZ(montoFinal_naranja.debito, "D", "Precio final con Debito");
-        resultadoCuotas.innerHTML += tarjetaDebitoYPlanZ(montoFinal_ahora.naranja, "Z", "Este monto en Point");
+        resultadoCuotas.innerHTML += crearTitulo("Naranja")
         resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja3, 3);
         resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja6, 6);
         
