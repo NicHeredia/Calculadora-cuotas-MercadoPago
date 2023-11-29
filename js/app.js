b667d12b754f3beb04b2730b8065c3e92e997367
Nicolas Heredia <nicoherediasp@gmail.com>
2023-11-29 12:58:25 -0300

    Agregada la muestra del porcentaje total en cada monto

--- js/app.js
+++ js/app.js
@@ -24,25 +24,38 @@ const costos_compra = {
     cincuenta: 1.815
 }
 
+const porcentajes = {
+    debito : 8,
+    cuotas1 : 13,
+    naranja2 : 47,
+    naranja3 : 62,
+    naranja6 : 120,
+    ahora3 : 34,
+    ahora6 : 59,
+    ahora12 : 126  
+}
+
 
 //Funciones que se encargan de crear las tarjetas y los titulos
-const tarjetaCuotas = (montoFinal,cuota) => {
+const tarjetaCuotas = (montoFinal,cuota, porcentaje) => {
     let plantilla = `<div class="cardCuota fade-in">
     <div class="cardCuota__numero">${cuota}</div>
     <div class="cardCuota__contenedorMontos">
       <p class="cardCuota__precioFinal">Precio Final $${Math.round(monto.value * montoFinal).toLocaleString("de-DE")}</p>
       <p class="cardCuota__precioCuotas">${cuota} cuotas de $${Math.round((monto.value * montoFinal) / cuota).toLocaleString("de-DE")}</p>
-    </div>`
+    </div>
+    <div class="cardCuota__porcentaje">${porcentaje}%</div>`
     return plantilla 
 }
 
-const tarjetaDebito = (montoFinal, cuota, nombre) => {
+const tarjetaDebito = (montoFinal, cuota, nombre, porcentaje) => {
     let plantilla = `<div class="cardCuota fade-in">
     <div class="cardCuota__numero">${cuota}</div>
     <div class="cardCuota__contenedorMontos">
       <p class="cardCuota__precioFinal">Precio Final $${Math.round(monto.value * montoFinal).toLocaleString("de-DE")}</p>
       <p class="cardCuota__precioCuotas">${nombre}</p>
-    </div>`
+    </div>
+    <div class="cardCuota__porcentaje">${porcentaje}%</div>`
     return plantilla 
 }
 
@@ -65,11 +78,11 @@ form.addEventListener("click", (e) =>{
         btnBorrar.style.display = "block";
     resultadoCuotas.innerHTML = "";
     resultadoCuotas.innerHTML += crearTitulo("Plan Ahora")
-    resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.cuotas1, "1", "Precio final 1 Cuota")
-    resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.debito, "D", "Precio final con Debito")
-    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas3, 3);
-    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas6, 6);
-    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas12, 12);
+    resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.cuotas1, "1", "Precio final 1 Cuota", porcentajes.cuotas1);
+    resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.debito, "D", "Precio final con Debito", porcentajes.debito);
+    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas3, 3, porcentajes.ahora3);
+    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas6, 6, porcentajes.ahora6);
+    resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_ahora.cuotas12, 12, porcentajes.ahora12);
     
     } else if (e.target.id === "btn-borrar"){
         btnBorrar.style.display = "none";
@@ -89,10 +102,10 @@ form.addEventListener("click", (e) =>{
         btnBorrar.style.display = "block";
         resultadoCuotas.innerHTML = "";
         resultadoCuotas.innerHTML += crearTitulo("Naranja");
-        resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.cuotas1, "1", "Precio final 1 Cuota")
-        resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja2, 2);
-        resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja3, 3);
-        resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja6, 6);
+        resultadoCuotas.innerHTML += tarjetaDebito(montoFinal_ahora.cuotas1, "1", "Precio final 1 Cuota", porcentajes.cuotas1);
+        resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja2, 2, porcentajes.naranja2);
+        resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja3, 3, porcentajes.naranja3);
+        resultadoCuotas.innerHTML += tarjetaCuotas(montoFinal_naranja.naranja6, 6, porcentajes.naranja6);
         
     } else if (e.target.id === "btn-costos") {
         btnBorrar.style.display = "block";
